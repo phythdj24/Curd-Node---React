facb2c578d58a7ffdc680ac943cc38faae070b3f
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-23 22:59:21 +0530

    DB

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,21 @@
+// db/index.js
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 
-
-
-
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        if (!process.env.MONGODB_URL) {
+            throw new Error("MongoDB URL is not defined in the environment variables.");
+        }
+
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URL, {
+            dbName: DB_NAME,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log(`\n MongoDB Connected || DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGO DB CONNECTION ERROR !", error);
+        console.log("MONGO DB CONNECTION ERROR !", error.message);
         process.exit(1);
     }
 };
