3b709a682e22263556e1eb8aca6202a3eb487c28
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-24 21:03:59 +0530

    chnages

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ const app = express()
 
 const PORT = 8000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/curd-app").then(()=> console.log("Databse connected Sucesfull")).catch(err => console.log(err))
+mongoose.connect("mongodb://127.0.0.1:27017/curd-app").then(()=> console.log("Databse connected Sucesfull")).catch(err => console.log(err, "Mongo err"))
 
 app.listen(PORT, ()=> console.log(`server is running on Port ${PORT}`))
