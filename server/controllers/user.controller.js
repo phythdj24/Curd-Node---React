516d75c9bde50ae530296f3905e74942f12aadb9
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-26 21:58:07 +0530

    created controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,6 +6,11 @@ export const create = async(req,res)=>{
     try {
 
         const userData = new User(req.body);
+        if(!userData){
+            return res.status(404).json(msg:"User  not found")
+        }
+
+        const savedData = await userdata.save
 
 
 
